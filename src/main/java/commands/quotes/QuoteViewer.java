5c6181e539d16ed84a117d0718818d17466ccab1
ChnP7 <Christian.Perla7@gmail.com>
2023-06-28 13:50:19 -0400

    Added documentation

--- src/main/java/commands/quotes/QuoteViewer.java
+++ src/main/java/commands/quotes/QuoteViewer.java
@@ -11,17 +11,33 @@ import java.util.*;
 import static botmilez.config.QUOTE_FILE_PREFIX;
 import static botmilez.config.QUOTE_FILE_SUFFIX;
 
-//TODO: Track changes to the json files and include into arrays maybe by calling a method to check and does so
-// before any of the reading ones.
+
+/**
+ * QuoteViewer class: A class that is responsible for the reading of JSON files
+ * that have quotes stored in them, and methods that return quote requests to
+ * a user that asked for them.
+ */
 public class QuoteViewer extends ListenerAdapter {
 
+    /* serverId -> server's quotes */
     Map<Long, List<QuoteContext>> quotesArrays;
+
+    /* serverId -> if quotes have been modified since last update */
     Map<Long, Boolean> isModified;
     public QuoteViewer() {
         quotesArrays = new HashMap<>();
         isModified = new HashMap<>();
     }
 
+
+    /**
+     * Reads from a json file containing quotes and converts this file
+     * into a list of QuoteContext objects that will be ready for viewing by
+     * server members.
+     *
+     * @param path JSON file location to read from
+     * @return a list of QuoteContext objects representing server quotes.
+     */
     public static List<QuoteContext> jsonToQuoteContext(String path) {
         JSONArray contextArray = (JSONArray)IO.readJson(path);
         List<QuoteContext> quoteContexts = new ArrayList<>();
@@ -56,32 +72,25 @@ public class QuoteViewer extends ListenerAdapter {
 
     }
 
-    public void setAsModified(long guildId) {
-        isModified.replace(guildId, true);
-    }
 
-    public void getRandomQuote(long guildId, MessageChannel channel) {
-
-        updateQuotesArrays(guildId);
-        List<QuoteContext> quoteContexts = quotesArrays.get(guildId);
-        if (quoteContexts.isEmpty()) {
-            channel.sendMessage("No quotes are stored in the bot!" +
-                    "You can add more quotes by using the add option.").queue();
-            return;
-        }
-        Random random = new Random();
-        int index = random.nextInt(quoteContexts.size());
-        QuoteContext quoteContext = quoteContexts.get(index);
-
-        quoteContext.sendQuoteContext(channel);
-    }
-
-    private void updateQuotesArrays(long guildId) {
+    /**
+     * Checks if the quotes of a server have either not been updated since a
+     * previous addition or deletion, or have not been loaded at all. This
+     * method will load the up-to-date quotes into the quote array for viewing
+     * if needed.
+     *
+     * @param guildId id of the server to load quotes from
+     * @return whether a needed update was successful or not
+     */
+    private boolean updateQuotesArrays(long guildId) {
         String path = QUOTE_FILE_PREFIX + guildId + QUOTE_FILE_SUFFIX;
 
         /* If quotes for this guild are not loaded, load them */
         if (!quotesArrays.containsKey(guildId)) {
             List<QuoteContext> quoteContexts = jsonToQuoteContext(path);
+            if (quoteContexts == null) {
+                return false;
+            }
             quotesArrays.put(guildId, quoteContexts);
             isModified.put(guildId, false);
         }
@@ -89,10 +98,57 @@ public class QuoteViewer extends ListenerAdapter {
         /* If quotes of this server were modified, update */
         if (isModified.get(guildId)) {
             List<QuoteContext> quoteContexts = jsonToQuoteContext(path);
+            if (quoteContexts == null) {
+                return false;
+            }
             quotesArrays.replace(guildId, quoteContexts);
             isModified.replace(guildId, false);
         }
+
+        return true;
     }
+
+
+    /**
+     * Called by other methods to indicate the quotes of a server have been modified in some way,
+     * to indicate to this class that the quotes array needs to be re-loaded with the
+     * newly updated quotes.
+     *
+     * @param guildId id of the server that has had its quotes modified
+     */
+    public void setAsModified(long guildId) {
+        isModified.replace(guildId, true);
+    }
+
+
+    /**
+     * Sends a randomly selected quote belonging to the server
+     * @param guildId server id to load quotes from
+     * @param channel channel in server to send message to
+     */
+    public void getRandomQuote(long guildId, MessageChannel channel) {
+
+        if (!updateQuotesArrays(guildId)) {
+            channel.sendMessage("An error has occurred. Cannot load quotes!")
+                    .queue();
+        }
+
+        List<QuoteContext> quoteContexts = quotesArrays.get(guildId);
+        if (quoteContexts.isEmpty()) {
+            channel.sendMessage("No quotes are stored in the bot!" +
+                    "You can add more quotes by using the add option.").queue();
+            return;
+        }
+
+        Random random = new Random();
+        int index = random.nextInt(quoteContexts.size());
+        QuoteContext quoteContext = quoteContexts.get(index);
+
+        quoteContext.sendQuoteContext(channel);
+    }
+
+
+
 
 
 }
