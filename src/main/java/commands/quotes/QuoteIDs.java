754bff10145c8c2163d60697ba400644fd6c461e
ChnP7 <Christian.Perla7@gmail.com>
2023-07-06 15:33:07 -0400

    Initial Commit - Stores ids for quote-related componenets

--- src/main/java/commands/quotes/QuoteIDs.java
+++ src/main/java/commands/quotes/QuoteIDs.java
@@ -1,2 +1,20 @@
-package commands.quotes;public class QuoteIDs {
+package commands.quotes;
+
+public class QuoteIDs {
+
+    /***************************************************
+     * StringSelectInteractions choices - viewing quotes
+     ***************************************************/
+
+    public static final String SELECT_MENU_VIEW = "choiceview";
+    public static final String SELECT_CHOICE_RANDOM = "random";
+    public static final String SELECT_CHOICE_ALL = "all";
+    public static final String SELECT_CHOICE_SEARCH = "search";
+
+
+    /******************************************************
+     * SlashCommandInteraction Buttons - add, view, delete
+     ******************************************************/
+    public static final String BUTTON_ID_ADD = "addquote";
+    public static final String BUTTON_ID_VIEW = "viewquote";
 }
