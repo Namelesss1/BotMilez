4abc7a8acf2575b342183a259091944ef8695861
Christian <Christian.Perla7@gmail.com>
2023-07-13 18:21:42 -0400

    Added characters stats functionality

--- src/main/java/commands/util/MkwStatsCommand.java
+++ src/main/java/commands/util/MkwStatsCommand.java
@@ -2,14 +2,13 @@ package commands.util;
 
 import mariokart.MKW;
 import commands.IBotCommand;
+import mariokart.MKWCharacter;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -66,7 +65,7 @@ public class MkwStatsCommand extends ListenerAdapter implements IBotCommand {
         if (event.getComponentId().equals(MENU_SELECT_STAT)) {
             if (event.getValues().get(0).equals(SELECT_VEHICLES)) {
                 event.editMessage("Select the weight class of vehicle").queue();
-                event.editSelectMenu(StringSelectMenu.create(MENU_SELECT_CLASS)
+                event.editSelectMenu(StringSelectMenu.create(MENU_SELECT_VEHICLE_CLASS)
                         .addOption("Light", SELECT_LIGHT_VEHICLE,
                                 "Lightweight vehicles")
                         .addOption("Medium", SELECT_MEDIUM_VEHICLE,
@@ -75,13 +74,25 @@ public class MkwStatsCommand extends ListenerAdapter implements IBotCommand {
                                 "Heavy vehicles")
                         .build()
                 ).queue();
+            }
 
+            if (event.getValues().get(0).equals(SELECT_CHARACTERS)) {
+                event.editMessage("Select the weight class of character").queue();
+                event.editSelectMenu(StringSelectMenu.create(MENU_SELECT_CHARACTER_CLASS)
+                        .addOption("Light", SELECT_LIGHT_CHARACTER,
+                                "Lightweight characters")
+                        .addOption("Medium", SELECT_MEDIUM_CHARACTER,
+                                "Medium characters")
+                        .addOption("Heavy", SELECT_HEAVY_CHARACTER,
+                                "Heavy characters")
+                        .build()
+                ).queue();
             }
         }
 
 
         /* After choosing weight class */
-        if (event.getComponentId().equals(MENU_SELECT_CLASS)) {
+        if (event.getComponentId().equals(MENU_SELECT_VEHICLE_CLASS)) {
             StringSelectMenu.Builder SSMBuilder = StringSelectMenu.create(MENU_SELECT_VEHICLE);
             event.editMessage("Select which vehicle you want.").queue();
 
@@ -111,8 +122,33 @@ public class MkwStatsCommand extends ListenerAdapter implements IBotCommand {
         }
 
 
+        if (event.getComponentId().equals(MENU_SELECT_CHARACTER_CLASS)) {
+            StringSelectMenu.Builder SSMBuilder = StringSelectMenu.create(MENU_SELECT_CHARACTER);
+            event.editMessage("Select which character you want.").queue();
 
-        /* After selecting specific vehicle/character */
+            WeightClass weightClass = WeightClass.ALL;
+            if (event.getValues().get(0).equals(SELECT_LIGHT_CHARACTER)) {
+                weightClass = WeightClass.LIGHT;
+            } else if (event.getValues().get(0).equals(SELECT_MEDIUM_CHARACTER)) {
+                weightClass = WeightClass.MEDIUM;
+            } else if (event.getValues().get(0).equals(SELECT_HEAVY_CHARACTER)) {
+                weightClass = WeightClass.HEAVY;
+            }
+
+            Map<String, MKWCharacter> characters =
+                    mkw.getCharacters(weightClass);
+            List<MKWCharacter> sortedCharacters = new ArrayList<>(characters.values());
+            Collections.sort(sortedCharacters);
+
+            for (MKWCharacter character : sortedCharacters) {
+                SSMBuilder.addOption(character.getName(), character.getName());
+            }
+            event.editSelectMenu(SSMBuilder.build()).queue();
+        }
+
+
+
+        /* After selecting specific vehicle*/
         if (event.getComponentId().equals(MENU_SELECT_VEHICLE)) {
             Map<String, MKWVehicle> vehicles =
                     mkw.getVehicles(WeightClass.ALL, VehicleType.ALL, DriftType.ALL);
@@ -126,6 +162,27 @@ public class MkwStatsCommand extends ListenerAdapter implements IBotCommand {
                     emBuilder.addField(vehicle.getAsField());
                     emBuilder.setImage(vehicle.getImgURL());
                     emBuilder.setFooter("Image Credit: " + vehicle.getImgcredit());
+                }
+            }
+
+            event.editMessage("Here are the stats.").queue();
+            event.getHook().editOriginalEmbeds(emBuilder.build()).queue();
+        }
+
+
+        /* After selecting specific character */
+        if (event.getComponentId().equals(MENU_SELECT_CHARACTER)) {
+            Map<String, MKWCharacter> characters = mkw.getCharacters(WeightClass.ALL);
+
+            EmbedBuilder emBuilder = new EmbedBuilder();
+            emBuilder.setColor(Color.BLUE);
+            emBuilder.setTitle("Character Information");
+
+            for (MKWCharacter character : characters.values()) {
+                if (event.getValues().get(0).equals(character.getName())) {
+                    emBuilder.addField(character.getAsField());
+                    emBuilder.setImage(character.getImgURL());
+                    emBuilder.setFooter("Image Credit: " + character.getImgcredit());
                 }
             }
 
