924cc604564854225a0262e54c10919b928ae898
Christian <Christian.Perla7@gmail.com>
2023-07-14 20:54:29 -0400

    Initial commit - Help command skeleton

--- src/main/java/commands/helper/HelpCommand.java
+++ src/main/java/commands/helper/HelpCommand.java
@@ -1,2 +1,31 @@
-package commands.helper;public class HelpCommand {
+package commands.helper;
+
+import commands.IBotCommand;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public class HelpCommand implements IBotCommand {
+
+    @Override
+    public String getName() {
+        return "help";
+    }
+
+    @Override
+    public String getDesc() {
+        return "Get help on how to use certain commands";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return null;
+    }
+
+    @Override
+    public void doAction(SlashCommandInteractionEvent event) {
+
+    }
+
 }
