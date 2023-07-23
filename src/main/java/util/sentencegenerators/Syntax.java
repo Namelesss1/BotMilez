43591b31ebdb28cde4b34b3ee0c56950e6ca7830
Christian <Christian.Perla7@gmail.com>
2023-07-23 15:50:37 -0400

    Added a sentence generator class, to be implemented

---
+++ src/main/java/util/sentencegenerators/Syntax.java
@@ -0,0 +1,9 @@
+package util.sentencegenerators;
+
+public class Syntax {
+
+    /* Existential Clause: "There [be] [a/quantifier/] [NP] ([possible VP]) | (CP) ... */
+    /* Skeletons of clause types, potential phrases, tense, etc. involved in each */
+    /* Methods to obtain phrases (NP,VP, etc.) with needed info passed as a parameter to obtain correct phrase format */
+    /* Store info on verbs - tense, transitivity, person, number etc. */
+}
