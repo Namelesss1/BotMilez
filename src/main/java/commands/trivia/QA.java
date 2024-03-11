dbb36ac3eecd89dbea892a661ce30c6abb536fa8
Nameless <no03name07@gmail.com>
2024-03-11 12:20:14 -0400

    QA as a separate class, add setters and flexible constructors

---
+++ src/main/java/commands/trivia/QA.java
@@ -0,0 +1,105 @@
+package commands.trivia;
+
+import java.util.List;
+
+/**
+ * This class represents all metadata representing a question in this trivia type
+ */
+public class QA {
+    private long id;
+    private String question;
+    private List<String> answers;
+    private long points;
+    private boolean was_asked;
+
+    public QA(long id, String ques, List<String> ans, long pts) {
+        this.id = id;
+        question = ques;
+        answers = ans;
+        points = pts;
+        was_asked = false;
+    }
+
+    public QA(long id) {
+        was_asked = false;
+        this.id = id;
+    }
+
+    public QA() {
+        was_asked = false;
+    }
+
+    
+    /**
+     * @param id sets the unique id of this question
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Sets the question prompt of this object
+     * @param ques question prompt
+     */
+    public void setQuestion(String ques) {
+        question = ques;
+    }
+
+    /**
+     * Sets the corresponding answers of this object
+     * @param answers list of all correct answers
+     */
+    public void setAnswer(List<String> answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * Sets the points worth of this question
+     * @param points how many points this question is worth
+     */
+    public void setPoints(long points) {
+        this.points = points;
+    }
+
+    /**
+     * Sets this question as already asked
+     */
+    public void set_asked() {
+        was_asked = true;
+    }
+
+    /**
+     * @return id of question
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @return String representing the question
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * @return list of possible answers to the question
+     */
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    /**
+     * @return how many points the question is worth
+     */
+    public long getPoints() {
+        return points;
+    }
+
+    /**
+     * @return true if question was already asked, false if not
+     */
+    public boolean wasAsked() {
+        return was_asked;
+    }
+}
